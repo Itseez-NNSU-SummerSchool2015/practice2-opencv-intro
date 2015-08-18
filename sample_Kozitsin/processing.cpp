e59288270ce7620dfc731b1459fcdaae902bc2b9
unknown <ss2015@ws-2k-110-n09.ccam.cmc.unn.net>
2015-08-18 14:30:42 +0300

    First preparation + random walk

---
+++ sample_Kozitsin/processing.cpp
@@ -0,0 +1,31 @@
+#include "processing.hpp"
+
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <stdlib.h>
+#include <time.h>
+
+using namespace cv;
+
+void Processing::processFrame(const cv::Mat& src, cv::Mat& dst)
+{
+	srand(time(NULL));
+
+    src.copyTo(dst);
+    int x = rand() % src.rows - 1;
+    int y = rand() % src.cols - 1;
+
+    while (x + 100 > 500 || y + 100 > 500)
+    {
+    	x = rand() % src.rows - 1;
+    	y = rand() % src.cols - 1;
+    }
+
+    cv::Rect region(x, y, 100, 100);
+    Mat roi = dst(region);
+
+    const int kSize = 11;
+    medianBlur(roi, roi, kSize);
+
+    rectangle(dst, region, Scalar(255, 0, 0));
+}
