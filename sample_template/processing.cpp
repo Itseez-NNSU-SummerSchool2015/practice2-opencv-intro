6e6bbdb9fb8b6f0abd7527a07f9dee0c5753b978
Moskalenko <ss2015@ws-2k-114-11n.ccam.cmc.unn.net>
2015-08-18 15:35:48 +0300

    Random_position

--- sample_template/processing.cpp
+++ sample_template/processing.cpp
@@ -1,14 +1,21 @@
 #include "processing.hpp"
 
+#include <time.h>
 #include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
+using namespace std;
 
 void Processing::processFrame(const cv::Mat& src, cv::Mat& dst)
 {
     src.copyTo(dst);
 
-    cv::Rect region(src.rows/4, src.cols/4, src.rows/2, src.cols/2);
+	time_t t;
+	t = time(0);
+
+    //cv::Rect region(src.rows/4, src.cols/4, src.rows/2, src.cols/2);
+	int a = clock()/100;
+	cv::Rect region((a*a)%(src.rows/2 - 1), a%(src.cols/2 - 1), src.rows/2, src.cols/2);
     Mat roi = dst(region);
 
     const int kSize = 11;
