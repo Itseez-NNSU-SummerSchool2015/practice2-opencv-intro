62f9cb486e44c13fafce3ae32dabd7456dd96343
unknown <ss2015@ws-2k-110-n04.ccam.cmc.unn.net>
2015-08-18 14:52:34 +0300

    add moving region on image

--- sample_ekaterina_maljutina/processing.cpp
+++ sample_ekaterina_maljutina/processing.cpp
@@ -4,15 +4,45 @@
 
 using namespace cv;
 
+int step = 0;
+int size = 0;
+
+int x = 0;
+int y = 0;
+
 void Processing::processFrame(const cv::Mat& src, cv::Mat& dst)
 {
+	size +=2;
     src.copyTo(dst);
+	cv::Rect region;
+	
+	if (( x + step + src.rows/2 + size < src.rows) 
+		&& (y + step + src.cols/2 + size  < src.cols))
+	{
+		
+		cv::Rect r(x + step , y + step, src.rows/2 + size, src.cols/2 + size);
+		region =r;
 
-    cv::Rect region(src.rows/4, src.cols/4, src.rows/2, src.cols/2);
+	}
+	else
+	{
+		//size = size - 5;
+		if (step != 0)
+			step = 0;
+		x=0;
+		y=0;
+		cv::Rect r(x ,y , src.rows/2 + size, src.cols/2 + size );
+		region =r;
+		size = 0;
+	}
+
     Mat roi = dst(region);
 
     const int kSize = 11;
     medianBlur(roi, roi, kSize);
 
     rectangle(dst, region, Scalar(255, 0, 0));
+
+	step = step + 2 ;
+	
 }
