d48b6b486080e822923eace30a1c0e6721fd4752
vihuhol <ss2015@ws-2k-110-n05.ccam.cmc.unn.net>
2015-08-18 15:05:22 +0300

    Add moving to median region

--- sample_semichev/processing.cpp
+++ sample_semichev/processing.cpp
@@ -3,16 +3,38 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
+int move = 0;
+bool isMovingRight = true;
 
 void Processing::processFrame(const cv::Mat& src, cv::Mat& dst)
 {
     src.copyTo(dst);
+    if (isMovingRight)
+        if (move >= 50)
+            isMovingRight = false;
+        else
+        {
+            cv::Rect region(src.rows/4 + move, src.cols/4, src.rows/2 + 10, src.cols/2 + 100);
+            move++;
+                Mat roi = dst(region);
 
-    cv::Rect region(src.rows/4, src.cols/4, src.rows/2, src.cols/2);
-    Mat roi = dst(region);
+            const int kSize = 11;
+            medianBlur(roi, roi, kSize);
 
-    const int kSize = 11;
-    medianBlur(roi, roi, kSize);
+            rectangle(dst, region, Scalar(255, 0, 0));
+        }
+    else
+        if (move <= -50)
+            isMovingRight = true;
+        else
+        {
+            cv::Rect region(src.rows/4 + move, src.cols/4, src.rows/2 - 10, src.cols/2 - 100);
+            move--;
+                Mat roi = dst(region);
 
-    rectangle(dst, region, Scalar(255, 0, 0));
+            const int kSize = 11;
+            medianBlur(roi, roi, kSize);
+
+            rectangle(dst, region, Scalar(255, 0, 0));
+        }
 }
