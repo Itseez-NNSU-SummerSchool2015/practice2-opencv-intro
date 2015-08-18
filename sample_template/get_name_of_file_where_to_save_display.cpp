0cdf4cc5be34da84be56c0f4eb55c278799ffa68
unknown <ss2015@ws-2k-110-n08.ccam.cmc.unn.net>
2015-08-18 18:16:10 +0300

    More fixxes.

--- sample_template/get_name_of_file_where_to_save_display.cpp
+++ sample_template/get_name_of_file_where_to_save_display.cpp
@@ -4,12 +4,11 @@
 
 std::string  get_name_of_file_where_to_save_display(){
 	time_t lt;
- lt = time(NULL);
- std::string s=ctime(&lt);
- s[s.size()-1]='.';
- for(int i=0;i<s.size();i++){if(s[i]==' ') s[i]='_';}
- for(int i=0;i<s.size();i++){if(s[i]==':') s[i]='_';}
- s=s+"png";
-	//std::cout<<s;
+	lt = time(NULL);
+	std::string s=ctime(&lt);
+	s[s.size()-1]='.';
+	for(int i=0;i<s.size();i++){if(s[i]==' ') s[i]='_';}
+	for(int i=0;i<s.size();i++){if(s[i]==':') s[i]='_';}
+	s=s+"png";
 	return s;
-}
+	}
