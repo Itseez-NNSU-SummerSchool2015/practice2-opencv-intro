39957e3daa35d9652a7f1ed4c78d1ce4f0291ba4
ag <ss2015@ws-2k-114-10n.ccam.cmc.unn.net>
2015-08-18 16:05:15 +0300

    Added size changing

--- sample_template/processing.cpp
+++ sample_template/processing.cpp
@@ -7,10 +7,13 @@ using namespace cv;
 void Processing::processFrame(const cv::Mat& src, cv::Mat& dst)
 {
     src.copyTo(dst);
-	int x=0, y=0;
-	x=std::rand()%src.rows/2+src.rows/8;
-	y=std::rand()%src.cols/2+src.cols/8;
-	cv::Rect region(x, y, src.rows/4, src.cols/4);
+	float x=0, y=0;
+	int xSize=0, ySize=0;
+	x=std::rand()%src.rows/2+src.rows/16;
+	y=std::rand()%src.cols/2+src.cols/16;
+	xSize=std::rand()%src.rows/8+src.rows/16;
+	ySize=std::rand()%src.cols/8+src.cols/16;
+	cv::Rect region(x, y, xSize, ySize);
 	Mat roi = dst(region);
 	const int kSize = 11;
 	medianBlur(roi, roi, kSize);
